595230e39ee6e858b57273cce55a7ac64b8672d7
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-02 20:03:36 +0200

    function implementation with tuple

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let width1 = 20;
-    let height1 = 30;
+    let rect1 = (20, 30);
 
-    println!("Area: {}", area(width1, height1));
+    println!("Area: {}", area(rect1));
 }
 
-fn area(w: u32, h: u32) -> u32 {
-    w * h
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }

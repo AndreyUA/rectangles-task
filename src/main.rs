3a411fe8a1493fe5a7646f007d93edd31a882db2
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-02 20:49:44 +0200

    debuggin of structures

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
         width: 20,
-        height: 30,
+        height: dbg!(30 * scale),
     };
 
     println!("Area: {}", area(&rect1));
 
-    println!(
-        "Test of var. Width: {}, height: {}",
-        rect1.width, rect1.height
-    );
+    dbg!(&rect1);
+
+    println!("Test of rect1: {:#?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {

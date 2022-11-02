22c79428bf368285affedd7b385a6fa2233494d6
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-02 19:57:32 +0200

    common function implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 20;
+    let height1 = 30;
+
+    println!("Area: {}", area(width1, height1));
+}
+
+fn area(w: u32, h: u32) -> u32 {
+    w * h
 }

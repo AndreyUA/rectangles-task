37978908049c5c23b9f2545acbb444754412d62f
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-03 20:25:17 +0200

    associated function without self as a param

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ impl Rectangle {
     fn can_hold(&self, another_rectangle: &Rectangle) -> bool {
         self.width > another_rectangle.width && self.height > another_rectangle.height
     }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -32,4 +39,8 @@ fn main() {
     println!("Test of rect1: {:#?}", rect1);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+    let square = Rectangle::square(60);
+
+    println!("Test of square: {:#?}", square);
 }
